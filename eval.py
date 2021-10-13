66152382670380a970208f94f8ee4d688efd4437
thomasoliver2021 <70597512+thomasoliver2021@users.noreply.github.com>
2021-10-13 01:37:28 -0400

    Major retraining and API calls

--- eval.py
+++ eval.py
@@ -1,25 +1,32 @@
 from tokenizer import Tokenizer
 from transformers import AutoModelWithLMHead
-
+import itertools
 import torch
 import time
 
 tokenizer = Tokenizer()
-model = AutoModelWithLMHead.from_pretrained('output-small')
+model = AutoModelWithLMHead.from_pretrained("chicaaago/coomaa_sensei")
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
-file_name = "./logs/log_" + timestr
+file_name = "./logs/log_" + timestr + ".txt"
 file = open(file_name, "w", encoding='utf-8')
 
-# Let's chat for 4 lines
-for step in range(3):
+print("Hello, welcome to the alpha version of クマー先生!")
+print("Enter \"q\" to stop chatting at any time.")
+
+count = 0
+for i in range(3):
     # encode the new user input, add the eos_token and return a tensor in Pytorch
     user_text = input(">> User: ")
+    if user_text == "q":
+        print("先生: じゃまた！")
+        file.close()
+        exit()
     new_user_input_ids = tokenizer.encode(user_text + tokenizer.eos_token, return_tensors='pt')
     file.write("User: " + user_text + "\n")
-    
+
     # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if count > 0 else new_user_input_ids
 
     # generated a response while limiting the total chat history to 1000 tokens, 
     chat_history_ids = model.generate(
@@ -30,11 +37,34 @@ for step in range(3):
         top_k=100, 
         top_p=0.7,
         temperature=0.8
-    )
-
+    ) 
+    
     # pretty print last ouput tokens from bot
     bot_text = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0])
     print("先生: {}".format(bot_text))
     file.write("Bot: " + bot_text + "\n")
 
-file.close()
+    count += 1
+
+    if count > 2:
+        to_seprate = chat_history_ids.tolist()
+        to_seprate = to_seprate[0]
+
+        size = len(to_seprate)
+        idx_list = [idx + 1 for idx, val in
+            enumerate(to_seprate) if val == 0]
+        
+        res = [to_seprate[i: j] for i, j in
+               zip([0] + idx_list, idx_list + 
+               ([size] if idx_list[-1] != size else []))]
+
+        del res[0]
+        del res[0]
+
+        final = itertools.chain.from_iterable(res)
+        final = list(final)
+
+        final = torch.IntTensor(final)
+        chat_history_ids = torch.unsqueeze(final, 0)
+
+        print(list(chat_history_ids))
--- tokenizer.py
+++ tokenizer.py
@@ -1,32 +1,57 @@
 import json
-import fugashi
 import tensorflow as tf
 import torch
+from math import log
 
 class Tokenizer:
     def __init__(self) -> None:
         self.eos_token = "<|endoftext|>"
-        self.eos_token_id = 2
-        self._tagger = fugashi.Tagger()
+        self.eos_token_id = 0
+        self.name = "オリバー"
 
+        # Taken from https://stackoverflow.com/questions/8870261/how-to-split-text-without-spaces-into-list-of-words
+        self.words = open("word_list.txt", encoding="utf-8").read().split()
+        self.wordcost = dict((k, log((i+1)*log(len(self.words)))) for i,k in enumerate(self.words))
+        self.maxword = max(len(x) for x in self.words)
+
+    # Separate the words
+    # Taken from https://stackoverflow.com/questions/8870261/how-to-split-text-without-spaces-into-list-of-words
+    def segment(self, s):
+        def best_match(i):
+            candidates = enumerate(reversed(cost[max(0, i-self.maxword):i]))
+            return min((c + self.wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
+
+        cost = [0]
+        for i in range(1,len(s)+1):
+            c,k = best_match(i)
+            cost.append(c)
+
+        out = []
+        i = len(s)
+        while i>0:
+            c,k = best_match(i)
+            assert c == cost[i]
+            out.append(s[i-k:i])
+            i -= k
+
+        return " ".join(reversed(out))
+
+    # Encode the user's text
     def encode(self, text, return_tensors=None):
+        if self.name in text:
+            text = text.replace(self.name, "<|name|>")
         output_ids = []
-        words = [word.surface for word in self._tagger(text)]
-        it = iter(words)
-        words = []
-        for word in it:
-            special_token = ""
-            if word == "<":
-                special_token += word
-                while word != ">":
-                    word = next(it)
-                    special_token += word
-                words.append(special_token)
-            else:
-                words.append(word)
+        words = self.segment(text).split()
         for word in words:
-            with open('./tokenized_data/vocab.json') as f:
+            with open('vocab.json', encoding='utf-8') as f:
                 vocab = json.load(f)
+                if word not in vocab:
+                    print("ERROR: Word not found.")
+                    print("Issue: " + word)
+                    print("Full text: ", end="")
+                    print(words)
+                    print("Please note this error and send it to coomahsensei@gmail.com")
+                    exit()
                 for key, value in vocab.items():
                     if key == word:
                         output_ids.append(value)
@@ -40,15 +65,18 @@ class Tokenizer:
             output = output_ids
         return output
 
+    # Decode the bot's text
     def decode(self, ids):
         output_text = []
         for id in ids:
-            with open('./tokenized_data/vocab.json') as f:
+            with open('vocab.json', encoding='utf-8') as f:
                 vocab = json.load(f)
                 for key, value in vocab.items():
                     if value == id:
                         output_text.append(key)
         output = ''.join(output_text)
-        output = output.replace("<pad>", "")
+
+        if "<|name|>" in output:
+            output = output.replace("<|name|>", self.name)
         output = output.split("<|endoftext|>")
         return output[0]
